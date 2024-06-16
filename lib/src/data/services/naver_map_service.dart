cb376231b8a9c1e87920aa5db4ab058d4b39da2c
seojeong <sjytis14@naver.com>
2024-06-17 00:33:26 +0900

    [Feat] android, ios 위치 권한 요청 후 사용자 위치로 initPosition

--- lib/src/data/services/naver_map_service.dart
+++ lib/src/data/services/naver_map_service.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
+
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
 class NaverMapService {
   // 지도 초기화
@@ -10,5 +12,45 @@ class NaverMapService {
     await NaverMapSdk.instance.initialize(
         clientId: clientId, // 클라이언트 ID 설정
         onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed"));
+  }
+
+  // 현재 위치 가져오기
+  Future<Position?> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    log('get current location');
+
+    // 위치 서비스 활성화 확인
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    log("serviceEnabled $serviceEnabled");
+    if (!serviceEnabled) {
+      // 위치 서비스가 비활성화 상태일 때
+      return null;
+      // return Future.error('위치 서비스가 비활성화되어 있습니다.');
+    }
+
+    // 위치 권한 요청 및 상태 확인
+    permission = await Geolocator.checkPermission();
+    log('permission $permission');
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      log('request permission $permission');
+      if (permission == LocationPermission.denied) {
+        // 위치 권한이 거부되었을 때
+        return null;
+        // return Future.error('위치 권한이 거부되었습니다.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // 위치 권한이 영구적으로 거부되었을 때
+      return null;
+      // return Future.error('위치 권한이 영구적으로 거부되었습니다. 권한을 요청할 수 없습니다.');
+    }
+
+    // 위치 정보 가져오기
+    return await Geolocator.getCurrentPosition();
   }
 }
--- lib/src/ui/widgets/app/app_naver_map_widget.dart
+++ lib/src/ui/widgets/app/app_naver_map_widget.dart
@@ -5,7 +5,7 @@ import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:client/src/data/services/naver_map_service.dart';
-// import 'package:geolocator/geolocator.dart';
+import 'package:geolocator/geolocator.dart';
 
 final naverMapServiceProvider = Provider((ref) => NaverMapService());
 
@@ -14,10 +14,10 @@ final naverMapInitializationProvider = FutureProvider<void>((ref) async {
   await naverMapService.initialize();
 });
 
-// final currentLocationProvider = FutureProvider<Position>((ref) async {
-//   final naverMapService = ref.read(naverMapServiceProvider);
-//   return await naverMapService.getCurrentLocation();
-// });
+final currentLocationProvider = FutureProvider<Position?>((ref) async {
+  final naverMapService = ref.read(naverMapServiceProvider);
+  return await naverMapService.getCurrentLocation();
+});
 
 class NaverMapWidget extends ConsumerWidget {
   const NaverMapWidget({super.key});
@@ -25,43 +25,44 @@ class NaverMapWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final initialization = ref.watch(naverMapInitializationProvider);
-    // final currentLocation = ref.watch(currentLocationProvider);
+    final currentLocation = ref.watch(currentLocationProvider);
 
     return initialization.when(
       data: (_) {
-        // return currentLocation.when(
-        //   data: (position) {
-        return NaverMap(
-            options: const NaverMapViewOptions(
-              initialCameraPosition: NCameraPosition(
-                  target: NLatLng(36.3553177, 127.2981911), // 대전 삼성화재 연수원
-                  zoom: 10,
-                  bearing: 0,
-                  tilt: 0),
-              indoorEnable: true,
-              locationButtonEnable: false,
-              consumeSymbolTapEvents: false,
-              locale: Locale('ko', 'KR'),
-            ),
-            onMapReady: (controller) {
-              // log('current location $currentLocation');
-              log("onMapReady", name: "onMapReady");
-            });
+        return currentLocation.when(
+          data: (position) {
+            final initialPosition = position != null
+                ? NLatLng(position.latitude, position.longitude)
+                : const NLatLng(
+                    36.3553177, 127.2981911); // default : 대전 삼성화재 연수원
+            return NaverMap(
+                options: NaverMapViewOptions(
+                  initialCameraPosition: NCameraPosition(
+                      target: initialPosition, zoom: 17, bearing: 0, tilt: 0),
+                  indoorEnable: true,
+                  locationButtonEnable: false,
+                  consumeSymbolTapEvents: false,
+                  locale: const Locale('ko', 'KR'),
+                ),
+                onMapReady: (controller) {
+                  log('current location $currentLocation');
+                  log("onMapReady", name: "onMapReady");
+                });
+          },
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          error: (error, stack) => Center(
+            child: Text('Error: $error'),
+          ),
+        );
       },
       loading: () => const Center(
         child: CircularProgressIndicator(),
-      ),
+      ), // 초기화 중 로딩 표시
       error: (error, stack) => Center(
         child: Text('Error: $error'),
       ),
     );
-    // },
-    // loading: () => const Center(
-    //   child: CircularProgressIndicator(),
-    // ), // 초기화 중 로딩 표시
-    // error: (error, stack) => Center(
-    //   child: Text('Error: $error'),
-    // ),
-    // )
   }
 }
